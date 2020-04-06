61d64ef1911055661e0132bbac95e2bb2849a6e2
Zoltan MG <zoltanmoragarcia@gmail.com>
2020-04-06 14:12:06 -0500

    add return

--- punto-1.c
+++ punto-1.c
@@ -4,25 +4,26 @@
 
 int main(void)
 {
-    /*Declaración de Variables necesarias para el uso del getline*/
-    int bytes_read;
-    size_t size = 10;
-    char *string;
+	/*Declaración de Variables necesarias para el uso del getline*/
+	int bytes_read;
+	size_t size = 10;
+	char *string;
 
-    printf("$ ");
+	printf("$ ");
 
-    /*Asignación de tamaño a la bariable string*/
-    string = malloc(sizeof(char) * size);
+	/*Asignación de tamaño a la bariable string*/
+	string = malloc(sizeof(char) * size);
 
-    /*Uso de la función getline pasando los paramentros (dir del string, dir del tamaño, salida estandar)*/
-    bytes_read = getline(&string, &size, stdin);
+	/*Uso de la función getline pasando los paramentros
+	  (dir del string, dir del tamaño, salida estandar)*/
+	bytes_read = getline(&string, &size, stdin);
 
-    if(bytes_read == -1)
-        printf("erro\n");
-    else
-    {
-        printf("%s\n", string);
-        //printf("El tamaño en bytes es %i\n, bytes_read");
-    }
-    
+	if(bytes_read == -1)
+		printf("erro\n");
+	else
+	{
+		printf("%s\n", string);
+		//printf("El tamaño en bytes es %i\n, bytes_read");
+	}
+	return (0);
 }
