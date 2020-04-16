c0453eac3f4302f50c83cfc094bfde10f8bbc7db
Zoltan MG <zoltanmoragarcia@gmail.com>
2020-04-16 17:50:14 -0500

    a

--- zoltan/refacture/shell.h
+++ zoltan/refacture/shell.h
@@ -11,15 +11,28 @@
 
 extern char **environ;
 
-char **add_arr(char *str, char *command);
+char *space_eliminator(char *string);
 int _putchar(char c);
-void first_func(void);
-char *concatenate(char *str_g, char *path);
-int stat_func(char *command);
-char **create_arr(char *str_g);
-void free_all(char **arr);
-char *_getenv(char *words);
-char **simple_arr(char *command, int words);
 char *_strdup(char *str);
+void free_all(char **arr);
+int validator(char *string);
+int stat_func(char *command);
+int check_exit(char *string);
+void simple_print(char *str);
+int sp_tab_finder(char *str);
+char *shortener(char *string);
+void short_func(int *counter);
+int point_finder(char *string);
+void print_number(long int number);
+int _merge(char *args, char **env);
+char **add_arr(char *str, char *command);
+char *concatenate(char *args, char *path);
+char *_getenv(char *words, char **environ);
+char **simple_arr(char *command, int words);
+int print_env(char *string, char **environ);
+void print_error(char *av, int counter, char *command);
+char **absolute_path(char *args, char *av, int counter);
+char **create_arr(char *arguments, char **env, char *av, int counter);
+
 
 #endif
